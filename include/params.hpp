526b01c6b24a473cf70069a02475be1f23d106b0
Denis Lambolez <denis.lambolez@gmail.com>
2018-06-03 21:02:26 +0200

    Stable version for Visual Studio 2017

--- include/params.hpp
+++ include/params.hpp
@@ -17,22 +17,22 @@ namespace fs = std::filesystem:;
 
 class Params
 {
-  public:
-    Params(int ac, char* av[]);
-    ~Params();
+    public:
+        Params(int ac, char* av[]);
+        ~Params();
   
-  private:
-    int rows_;                                        // # of rows of the board
-    int columns_;                                     // # of columns of the board
-    int cellSize_;                                    // size in pixels of a cell (square) of the board 
-    bool verbose_;                                    // verbose output (toggle)
-    bool first_;                                      // stop at firts solution found (toggle)
-    bool stats_;                                      // save solving stats in csv file (toggle)
-    bool images_;                                     // save png images of the solutions (toggle)
-    std::string fillColor_;                           // HTML name of the fill color for the cells
-    std::string borderColor_;                         // HTML name of the border color for the cells  
-    std::unordered_map<std::string, int> piecesSet_;  // # of each piece to solve the puzzle
-    fs::path outputDir_;							  // directory where to save the png images
+    private:
+        int rows_;                                        // # of rows of the board
+        int columns_;                                     // # of columns of the board
+        int cellSize_;                                    // size in pixels of a cell (square) of the board 
+        bool verbose_;                                    // verbose output (toggle)
+        bool first_;                                      // stop at firts solution found (toggle)
+        bool stats_;                                      // save solving stats in csv file (toggle)
+        bool images_;                                     // save png images of the solutions (toggle)
+        std::string fillColor_;                           // HTML name of the fill color for the cells
+        std::string borderColor_;                         // HTML name of the border color for the cells  
+        std::unordered_map<std::string, int> piecesSet_;  // # of each piece to solve the puzzle
+        fs::path outputDir_;							  // directory where to save the png images
 };
 
 #endif // _PARAMS_HPP
